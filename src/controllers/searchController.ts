9abf854a534fe34c8ba729c1d58e3db12b5e2ff2
Filipe Santos <filipedemogi@gmail.com>
2022-04-14 16:29:06 -0300

    Projeto Final

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,20 @@
 import { Response, Request } from "express";
+import { createMenuObject } from "../helpers/createMenuObject";
+import { Pet } from "../models/pet";
 
 export const search = (req:Request, res:Response) => {
-    res.send('Busca');
+    let query = req.query.q as string;
+
+    //!query ? res.redirect('/') : false;
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pet.getFromName(query);
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 }
